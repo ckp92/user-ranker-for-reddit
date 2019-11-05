17520e17fbd7ee8d2b5df32ba14ab8a9134d681c
Chintan Patel <chintan_patel818@hotmail.co.uk>
2019-11-05 11:14:58 +0000

    add CRA client and configure start scripts for development environment

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.get('/', (req, res) => res.send({ hello: 'World!' }));
 
+app.get('/api', (req, res) =>
+  res.send({ apiHome: 'Welcome to the API route' })
+);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Listening @ ${PORT}`));
