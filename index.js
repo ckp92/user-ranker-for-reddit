06a7cf448c7f5847cf6137208861beb8ba6f2c2c
Chintan Patel <chintan_patel818@hotmail.co.uk>
2019-11-05 11:16:50 +0000

    remove "/" route from server so it defaults to client

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send({ hello: 'World!' }));
-
 app.get('/api', (req, res) =>
   res.send({ apiHome: 'Welcome to the API route' })
 );
