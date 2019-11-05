dd811a05c50d2a8579874cb3c7163f3c0e6f9d78
Chintan Patel <chintan_patel818@hotmail.co.uk>
2019-11-05 11:55:00 +0000

    make express look for CRA routes if it doesn't recognize them

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/api', (req, res) =>
   res.send({ apiHome: 'Welcome to the API route' })
 );
 
-const PORT = process.env.PORT || 5000;
+// make express behave correctly in production environment
+if (process.env.NODE_ENV === 'production') {
+  // tell express to look for files in 'client/build' if it can't find them
+  app.use(express.static('client/build'));
 
+  // tell express to serve up 'index.html' if it doesn't recignoze the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening @ ${PORT}`));
