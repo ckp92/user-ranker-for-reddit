13c006252d5aeac2d0487fad3ae650048ba641eb
Chintan Patel <chintan_patel818@hotmail.co.uk>
2019-11-18 12:37:35 +0000

    fix bug so headerNotification stays closed when user closes it (until app refreshes)

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-import App from './components/App';
-import reducers from './reducers';
+import App from "./components/App";
+import reducers from "./reducers";
 
+// redux devtools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// store
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.querySelector('#root')
+  document.querySelector("#root")
 );
--- client/src/reducers/headerNotificationReducer.js
+++ client/src/reducers/headerNotificationReducer.js
@@ -1,10 +1,10 @@
-import { CLOSE_HEADER_NOTIFICATION } from '../actions/types';
+import { CLOSE_HEADER_NOTIFICATION } from "../actions/types";
 
 export default (state = true, action) => {
   switch (action.type) {
     case CLOSE_HEADER_NOTIFICATION:
       return false;
     default:
-      return true;
+      return state;
   }
 };
